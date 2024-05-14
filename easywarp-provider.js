c8e118d9b331500691ab7c14b3d32888441bf6fb
keiko233 <i@elaina.moe>
2024-05-14 17:38:15 +0800

    init

---
+++ easywarp-provider.js
@@ -0,0 +1,26 @@
+// Define the `main` function
+
+function main(params) {
+  const extra = {
+    "proxy-providers": {
+      warp: {
+        type: "http",
+        url: "https://neko-warp.nloli.xyz/neko_warp.yaml",
+        interval: 3600,
+        path: "./proxy_providers/neko_warp.yaml",
+        override: {
+          "additional-prefix": "WARP ",
+        },
+      },
+    },
+  };
+
+  extra["proxy-groups"] = proxy_groups.map((item) => {
+    return {
+      ...item,
+      use: ["warp"],
+    };
+  });
+
+  return { ...params, ...extra };
+}
---
+++ useself.js
@@ -0,0 +1,268 @@
+// Define the `main` function
+
+function main(params) {
+  const proxies = params.proxies;
+
+  const extra = {
+    "rule-providers": {
+      reject: {
+        type: "http",
+        behavior: "domain",
+        url: "https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/reject.txt",
+        path: "./ruleset/reject.yaml",
+        interval: 86400,
+      },
+      apple: {
+        type: "http",
+        behavior: "domain",
+        url: "https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/apple.txt",
+        path: "./ruleset/apple.yaml",
+        interval: 86400,
+      },
+      google: {
+        type: "http",
+        behavior: "domain",
+        url: "https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/google.txt",
+        path: "./ruleset/google.yaml",
+        interval: 86400,
+      },
+      proxy: {
+        type: "http",
+        behavior: "domain",
+        url: "https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/proxy.txt",
+        path: "./ruleset/proxy.yaml",
+        interval: 86400,
+      },
+      direct: {
+        type: "http",
+        behavior: "domain",
+        url: "https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/direct.txt",
+        path: "./ruleset/direct.yaml",
+        interval: 86400,
+      },
+      gfw: {
+        type: "http",
+        behavior: "domain",
+        url: "https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/gfw.txt",
+        path: "./ruleset/gfw.yaml",
+        interval: 86400,
+      },
+      "tld-not-cn": {
+        type: "http",
+        behavior: "domain",
+        url: "https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/tld-not-cn.txt",
+        path: "./ruleset/tld-not-cn.yaml",
+        interval: 86400,
+      },
+      telegramcidr: {
+        type: "http",
+        behavior: "ipcidr",
+        url: "https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/telegramcidr.txt",
+        path: "./ruleset/telegramcidr.yaml",
+        interval: 86400,
+      },
+      cncidr: {
+        type: "http",
+        behavior: "ipcidr",
+        url: "https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/cncidr.txt",
+        path: "./ruleset/cncidr.yaml",
+        interval: 86400,
+      },
+      lancidr: {
+        type: "http",
+        behavior: "ipcidr",
+        url: "https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/lancidr.txt",
+        path: "./ruleset/lancidr.yaml",
+        interval: 86400,
+      },
+      applications: {
+        type: "http",
+        behavior: "classical",
+        url: "https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/applications.txt",
+        path: "./ruleset/applications.yaml",
+        interval: 86400,
+      },
+      BanAD: {
+        type: "http",
+        behavior: "domain",
+        url: "https://github.com/ACL4SSR/ACL4SSR/raw/master/Clash/Providers/BanAD.yaml",
+        path: "./ruleset/BanAD.yaml",
+        interval: 86400,
+      },
+      BanEasyList: {
+        type: "http",
+        behavior: "domain",
+        url: "https://github.com/ACL4SSR/ACL4SSR/raw/master/Clash/Providers/BanEasyList.yaml",
+        path: "./ruleset/BanEasyList.yaml",
+        interval: 86400,
+      },
+      BanEasyListChina: {
+        type: "http",
+        behavior: "domain",
+        url: "https://github.com/ACL4SSR/ACL4SSR/raw/master/Clash/Providers/BanEasyListChina.yaml",
+        path: "./ruleset/BanEasyListChina.yaml",
+        interval: 86400,
+      },
+      BanEasyPrivacy: {
+        type: "http",
+        behavior: "domain",
+        url: "https://github.com/ACL4SSR/ACL4SSR/raw/master/Clash/Providers/BanEasyPrivacy.yaml",
+        path: "./ruleset/BanEasyPrivacy.yaml",
+        interval: 86400,
+      },
+      ProxyMedia: {
+        type: "http",
+        behavior: "domain",
+        url: "https://github.com/ACL4SSR/ACL4SSR/raw/master/Clash/Providers/ProxyMedia.yaml",
+        path: "./ruleset/ProxyMedia.yaml",
+        interval: 86400,
+      },
+      OpenAI: {
+        type: "http",
+        behavior: "classical",
+        url: "https://github.com/ACL4SSR/ACL4SSR/raw/master/Clash/Providers/OpenAi.yaml",
+        path: "./ruleset/OpenAI.yaml",
+        interval: 86400,
+      },
+      Microsoft: {
+        type: "http",
+        behavior: "domain",
+        url: "https://github.com/ACL4SSR/ACL4SSR/raw/master/Clash/Providers/Microsoft.yaml",
+        path: "./ruleset/Microsoft.yaml",
+        interval: 86400,
+      },
+      OneDrive: {
+        type: "http",
+        behavior: "domain",
+        url: "https://github.com/ACL4SSR/ACL4SSR/raw/master/Clash/Providers/OneDrive.yaml",
+        path: "./ruleset/OneDrive.yaml",
+        interval: 86400,
+      },
+      EHGallery: {
+        type: "http",
+        behavior: "domain",
+        url: "https://github.com/ACL4SSR/ACL4SSR/raw/master/Clash/Providers/Ruleset/EHGallery.yaml",
+        path: "./ruleset/EHGallery.yaml",
+        interval: 86400,
+      },
+    },
+  };
+
+  const sshport_rules = ["DST-PORT,22,SSH Port", "DST-PORT,29418,SSH Port"];
+
+  const extra_rules = [
+    ...sshport_rules,
+    "RULE-SET,apple,Apple",
+    "RULE-SET,telegramcidr,Telegram",
+    "RULE-SET,OpenAI,OpenAI",
+    "RULE-SET,google,Google",
+    "RULE-SET,Microsoft,Microsoft",
+    "RULE-SET,OneDrive,OneDrive",
+    "RULE-SET,ProxyMedia,ProxyMedia",
+    "RULE-SET,EHGallery,EHGallery",
+    "RULE-SET,BanAD,Shield",
+    "RULE-SET,BanEasyList,Shield",
+    "RULE-SET,BanEasyListChina,Shield",
+    "RULE-SET,BanEasyPrivacy,Privacy",
+    "RULE-SET,lancidr,DIRECT",
+    "RULE-SET,applications,DIRECT",
+    "RULE-SET,cncidr,DIRECT",
+    "RULE-SET,reject,REJECT",
+    "RULE-SET,gfw,Proxies",
+    "RULE-SET,tld-not-cn,Proxies",
+    "MATCH,Proxies",
+  ];
+
+  const getProxies = (apper) => {
+    if (Array.isArray(apper)) {
+      return ["DIRECT", ...apper, ...proxies.map((item) => item.name)];
+    } else {
+      return ["DIRECT", ...proxies.map((item) => item.name)];
+    }
+  };
+
+  const proxy_groups = [
+    {
+      name: "Selected",
+      type: "select",
+      proxies: getProxies(),
+      icon: "https://cdn-icons-png.flaticon.com/512/16222/16222029.png",
+    },
+    {
+      name: "SSH Port",
+      type: "select",
+      proxies: ["Selected", ...getProxies()],
+      icon: "https://cdn-icons-png.flaticon.com/512/5136/5136897.png",
+    },
+    {
+      name: "Apple",
+      type: "select",
+      proxies: getProxies(["Selected"]),
+      icon: "	https://cdn-icons-png.flaticon.com/128/179/179309.png",
+    },
+    {
+      name: "Telegram",
+      type: "select",
+      proxies: ["Selected", ...getProxies()],
+      icon: "https://cdn-icons-png.flaticon.com/512/2111/2111646.png",
+    },
+    {
+      name: "Google",
+      type: "select",
+      proxies: ["Selected", ...getProxies()],
+      icon: "https://cdn-icons-png.flaticon.com/512/2702/2702602.png",
+    },
+    {
+      name: "Microsoft",
+      type: "select",
+      proxies: ["Selected", ...getProxies()],
+      icon: "https://cdn-icons-png.flaticon.com/512/732/732221.png",
+    },
+    {
+      name: "OneDrive",
+      type: "select",
+      proxies: getProxies(["Selected"]),
+      icon: "https://cdn-icons-png.flaticon.com/512/873/873136.png",
+    },
+    {
+      name: "OpenAI",
+      type: "select",
+      proxies: ["Selected", ...getProxies()],
+      icon: "https://cdn-icons-png.flaticon.com/512/11592/11592357.png",
+    },
+    {
+      name: "ProxyMedia",
+      type: "select",
+      proxies: ["Selected", ...getProxies()],
+      icon: "https://cdn-icons-png.flaticon.com/128/6404/6404519.png",
+    },
+    {
+      name: "EHGallery",
+      type: "select",
+      proxies: ["Selected", ...getProxies()],
+      icon: "https://cdn-icons-png.flaticon.com/512/9171/9171530.png",
+    },
+    {
+      name: "Proxies",
+      type: "select",
+      proxies: ["Selected", ...getProxies()],
+      icon: "https://cdn-icons-png.flaticon.com/512/9985/9985721.png",
+    },
+    {
+      name: "Shield",
+      type: "select",
+      proxies: ["REJECT", "DIRECT"],
+      icon: "https://cdn-icons-png.flaticon.com/512/9645/9645004.png",
+    },
+    {
+      name: "Privacy",
+      type: "select",
+      proxies: ["REJECT", "DIRECT"],
+      icon: "	https://cdn-icons-png.flaticon.com/512/4413/4413865.png",
+    },
+  ];
+
+  extra.rules = extra_rules;
+  extra["proxy-groups"] = proxy_groups;
+  return { ...params, ...extra };
+}
